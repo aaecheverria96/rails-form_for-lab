c283c558ad87c2211b1208c6fffb3e52e378f846
Your Username <you@example.com>
2021-03-24 22:44:41 -0400

    commit tree a85054e070c8a5197e9adc2ee5b87568a53af3b7

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,10 +12,18 @@ def create
     redirect_to school_class_path(@school_class)
 end 
 
-def update 
+def edit 
     @school_class = SchoolClass.find(params[:id]) 
-    @school_class.update(params.require(:school_class).permit(:title, :room_number)) 
-    redirect_to school_class_path(@school_class) 
+end 
+
+def update 
+ @school_class = SchoolClass.find(params[:id]) 
+ @school_class.update(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+ redirect_to school_class_path(@school_class)
+end 
+
+def show 
+    @school_class = SchoolClass.find(params[:id])
 end 
 
 
